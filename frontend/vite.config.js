99a614731c33ee518a7ed93fb752fb464544ef6d
Pablo-Saave <pablo.saavedra2201@alumnos.ubiobio.cl>
2026-02-20 21:28:06 -0300

    cambio archivo vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Cultura_Local/',   // 👈 Ajuste para GitHub Pages
+  base: '/Cultura_Local/',   
 
   plugins: [react()],
 
