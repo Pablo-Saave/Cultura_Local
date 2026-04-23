d38943412179e326c04fc00bdec918480be83701
Pablo-Saave <pablo.saavedra2201@alumnos.ubiobio.cl>
2026-04-23 19:46:44 -0400

    correciones front

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -159,7 +159,7 @@ function Blog() {
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
             {postsFiltrados.map((post) => (
               <a
                 key={post._id} 
